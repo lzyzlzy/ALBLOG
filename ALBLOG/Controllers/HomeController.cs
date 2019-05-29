1d89521794b4f91bd77550b45516c566bd789960
Andrew.Liu <317369453@qq.com>
2019-05-29 09:24:05 +0800

    重构 去除postcontroller增加根据tag查找post

--- ALBLOG/Controllers/HomeController.cs
+++ ALBLOG/Controllers/HomeController.cs
@@ -56,10 +56,50 @@ namespace ALBLOG.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Post(string title)
+        {
+            PostService postService = new PostService();
+            var post = postService.GetPost(i => i.Title == title);
+            if (post == null)
+                return RedirectToAction("Index", "Home");
+            string tags = "";
+            post.Tags.ForEach(i => tags += i + " ");
+            ViewData.Add("date", post.Date.AddHours(8).ToString("yyyy-MM-dd HH:mm"));
+            ViewData.Add("name", post.UserName);
+            ViewData.Add("tags", tags);
+            ViewData.Add("title", post.Title);
+            ViewData.Add("context", post.Context);
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Tag(string name, int index = 1)
+        {
+            int postNumOfOnePage = 10;
+            PostService postService = new PostService();
+            var allPosts = postService.GetAllPosts(i => i.IsDraft == false && i.Tags.Contains(name));
+            var posts = allPosts.Skip((index - 1) * 10).Take(postNumOfOnePage).ToList();
+            if (posts.Count == 0)
+            {
+                posts = allPosts.Take(10).ToList();
+                index = 1;
+            }
+            ViewData.Add("Title", name);
+            ViewData.Add("haveNext", allPosts.Count() > index * postNumOfOnePage ? "true" : "false");
+            ViewData.Add("haveLast", index > 1 ? "true" : "false");
+            ViewData.Add("posts", posts);
+            ViewData.Add("sum", allPosts.Count() % postNumOfOnePage != 0 ? allPosts.Count() / postNumOfOnePage + 1 : allPosts.Count() / postNumOfOnePage);
+            ViewData.Add("page", index);
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
     }
 }
