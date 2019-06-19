06b02775b793d5b6bd738d5b6cceae6d47412b5a
Andrew.Liu <317369453@qq.com>
2019-06-19 09:11:42 +0800

    Create Comment Model

---
+++ ALBLOG.Domain.Model/Comment.cs
@@ -0,0 +1,19 @@
+﻿using Common.MongoDBClient.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ALBLOG.Domain.Model
+{
+    [Collection("ALBLOG_Comment")]
+    public class Comment : DomainModel
+    {
+        public string Content { get; set; }
+        
+        public string Name { get; set; }
+
+        public string Date { get; set; }
+
+        public string IPAddress { get; set; }
+    }
+}
