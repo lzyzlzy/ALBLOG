a7938c0b98279523d9cca1679dd184e2d77adf9f
AndrewLiu <48308664+AndrewLiuZY@users.noreply.github.com>
2019-09-09 17:08:05 +0800

    增加ReturnModel

---
+++ ALBLOG.Domain.Model/Base/ReturnModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ALBLOG.Domain.Model
+{
+    public class ReturnModel
+    {
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+
+        public object Data { get; set; }
+    }
+}
