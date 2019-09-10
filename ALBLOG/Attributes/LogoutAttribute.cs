f89940e7af87316ce5988f82ee55af36aa10c3b3
AndrewLiu <48308664+AndrewLiuZY@users.noreply.github.com>
2019-09-10 18:00:22 +0800

    增加判断登录状态过滤器

---
+++ ALBLOG/Attributes/LogoutAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ALBLOG.Web.Attributes
+{
+    public class LogoutAttribute : ActionFilterAttribute, IActionFilter
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            context.HttpContext.Session.TryGetValue("username", out byte[] value);
+            if (value == null)
+                context.Result = new RedirectToActionResult("login", "Home", null);
+        }
+    }
+}
